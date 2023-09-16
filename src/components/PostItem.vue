c050719f4a35c3288249391cd52667d0218469c1
Eunbi Kim <eunbkim4@gmail.com>
2023-09-17 00:39:17 +0900

    feat: 대댓글 ui 수정

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="max-w-3xl mx-auto mt-10" v-if="post">
-    <!-- 게시글 제목 및 본문 -->
+    <!-- 게시글 영역 -->
     <div class="post-content pb-4">
       <div class="flex justify-between items-start mb-4">
+        <!-- 작성자 정보 -->
         <div class="flex items-center">
           <img
             :src="post.author.profileImage"
@@ -11,21 +12,24 @@
           />
           <span class="font-medium">{{ post.author.nickname }}</span>
         </div>
+        <!-- 게시글 작성일 -->
         <div>
           <span class="text-gray-500 text-sm">{{ formatDate(post.createdAt) }}</span>
         </div>
       </div>
+      <!-- 게시글 제목과 내용 -->
       <h2 class="text-2xl font-bold mb-4">{{ post.title }}</h2>
       <p class="mb-4">{{ post.content }}</p>
+      <!-- 좋아요 버튼 -->
       <button
         @click="toggleLikePost"
         class="bg-blue-500 text-white px-3 py-1 rounded-full hover:bg-blue-600 transition-colors"
       >
-        <i class="fas fa-thumbs-up"></i>
-        {{ post.likedUserIds.length }}
+        <i class="fas fa-thumbs-up"></i> {{ post.likedUserIds.length }}
       </button>
     </div>
 
+    <!-- 댓글 입력 영역 -->
     <div class="flex items-center my-4">
       <input
         class="flex-grow p-3 rounded-lg border border-gray-300 focus:outline-none"
@@ -40,12 +44,13 @@
       </button>
     </div>
 
-    <!-- 댓글 목록 -->
-    <div class="comments">
-      <div v-for="comment in post.comments" :key="comment.id" class="py-2 px-4">
-        <div :style="{ paddingLeft: comment.depth === 1 ? '40px' : '0px' }" class="border-b">
-          <div class="flex justify-between items-start mb-4">
-            <div class="flex items-center">
+    <!-- 댓글 목록 영역 -->
+    <div class="comments mt-10">
+      <div v-for="comment in post.comments" :key="comment.id" class="my-2">
+        <div class="border-b">
+          <div class="flex justify-between items-start">
+            <!-- 댓글 작성자 정보 -->
+            <div class="flex items-center mb-2">
               <img
                 :src="comment.author.profileImage"
                 alt="Author's profile image"
@@ -53,14 +58,12 @@
               />
               <span class="font-medium">{{ comment.author.nickname }}</span>
             </div>
+            <!-- 댓글 버튼들(대댓글, 좋아요, 수정, 삭제) -->
             <div>
-              <!-- 대댓글 버튼 추가 -->
-              <template v-if="comment.depth === 0">
-                <button @click="toggleReplyForm(comment.id)" class="mr-2 text-gray-600">
-                  <i class="fas fa-comment-dots"></i>
-                </button>
-              </template>
-              <button @click="toggleLikeComment(comment)" class="text-gray-600">
+              <button @click="toggleReplyForm(comment.id)" class="mr-2 text-gray-500">
+                <i class="fas fa-comment-dots"></i>
+              </button>
+              <button @click="toggleLikeComment(comment)" class="text-gray-500">
                 <i class="fas fa-thumbs-up mr-1" :class="{ 'text-blue-500': isLiked(comment) }"></i>
                 <span>{{ comment.likedUserIds.length }}</span>
               </button>
@@ -80,6 +83,7 @@
               </template>
             </div>
           </div>
+          <!-- 댓글 수정 영역 -->
           <template v-if="editingCommentId === comment.id">
             <textarea
               v-model="editingCommentContent"
@@ -100,29 +104,77 @@
               </button>
             </div>
           </template>
+          <!-- 댓글 내용 -->
           <template v-else>
             <p class="text-gray-700 mb-2 mt-2">{{ comment.content }}</p>
           </template>
+          <!-- 댓글 작성일 -->
           <div class="text-sm text-gray-500 mb-2">{{ formatDate(comment.createdAt) }}</div>
-
-          <!-- 조건부 렌더링으로 대댓글 입력 폼 표시 -->
-          
-          <div
-            v-if="comment.depth === 0 && activeReplyForm === comment.id"
-          >
-          <div class="flex items-center my-4">
-            <input
-              class="flex-grow p-3 rounded-lg border border-gray-300 focus:outline-none"
-              v-model="newReply[comment.id]"
-              placeholder="대댓글을 입력하세요..."
-            />
-            <button
-              @click="addReplyComment(comment.id)"
-              class="ml-4 px-5 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600 transition-colors"
-            >
-              전송
-            </button>
+          <!-- 대댓글 입력 영역 -->
+          <div v-if="activeReplyForm === comment.id">
+            <div class="flex items-center my-4">
+              <input
+                class="flex-grow p-3 rounded-lg border border-gray-300 focus:outline-none"
+                v-model="newReply[comment.id]"
+                placeholder="대댓글을 입력하세요..."
+              />
+              <button
+                @click="addReplyComment(comment.id)"
+                class="ml-4 px-5 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600 transition-colors"
+              >
+                전송
+              </button>
+            </div>
           </div>
+        </div>
+        <!-- 대댓글 목록 영역 -->
+        <div
+          v-for="childComment in comment.children"
+          :key="childComment.id"
+          class="ml-8 my-4 border-b"
+        >
+          <!-- 화살표 아이콘 -->
+          <div class="relative">
+            <i
+              class="fa-solid fa-turn-up fa-rotate-90 absolute top-0 left-[-1.5rem] text-gray-500"
+            ></i>
+            <!-- 대댓글 내용 표시 부분 -->
+            <div class="flex justify-between items-start mb-4">
+              <div class="flex items-center">
+                <img
+                  :src="childComment.author.profileImage"
+                  alt="Author's profile image"
+                  class="w-6 h-6 rounded-full mr-2"
+                />
+                <span class="font-medium">{{ childComment.author.nickname }}</span>
+              </div>
+              <div>
+                <!-- 대댓글 관련 버튼(좋아요, 수정, 삭제) -->
+                <button @click="toggleLikeComment(comment)" class="text-gray-500">
+                  <i
+                    class="fas fa-thumbs-up mr-1"
+                    :class="{ 'text-blue-500': isLiked(comment) }"
+                  ></i>
+                  <span>{{ comment.likedUserIds.length }}</span>
+                </button>
+                <template v-if="isCommentAuthor(comment)">
+                  <button
+                    @click="startEditing(comment)"
+                    class="ml-2 text-yellow-400 hover:text-yellow-600"
+                  >
+                    <i class="fas fa-edit"></i>
+                  </button>
+                  <button
+                    @click="deleteComment(comment.id)"
+                    class="ml-2 text-red-600 hover:text-red-800"
+                  >
+                    <i class="fas fa-trash-alt"></i>
+                  </button>
+                </template>
+              </div>
+            </div>
+            <p class="text-gray-700 mb-2 mt-2">{{ childComment.content }}</p>
+            <div class="text-sm text-gray-500 mb-2">{{ formatDate(childComment.createdAt) }}</div>
           </div>
         </div>
       </div>
@@ -236,9 +288,13 @@ export default {
     },
     formatDate(dateString) {
       const date = new Date(dateString)
-      return `${date.getFullYear()}/${
-        date.getMonth() + 1
-      }/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`
+      const year = date.getFullYear()
+      const month = String(date.getMonth() + 1).padStart(2, '0')
+      const day = String(date.getDate()).padStart(2, '0')
+      const hours = String(date.getHours()).padStart(2, '0')
+      const minutes = String(date.getMinutes()).padStart(2, '0')
+
+      return `${year}/${month}/${day} ${hours}:${minutes}`
     },
     toggleReplyForm(commentId) {
       if (this.activeReplyForm === commentId) {
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -131,9 +131,13 @@ export default {
     },
     formatDate(dateString) {
       const date = new Date(dateString)
-      return `${date.getFullYear()}/${
-        date.getMonth() + 1
-      }/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`
+      const year = date.getFullYear()
+      const month = String(date.getMonth() + 1).padStart(2, '0')
+      const day = String(date.getDate()).padStart(2, '0')
+      const hours = String(date.getHours()).padStart(2, '0')
+      const minutes = String(date.getMinutes()).padStart(2, '0')
+
+      return `${year}/${month}/${day} ${hours}:${minutes}`
     },
     truncateText(text, maxLength) {
       return text.length > maxLength ? text.slice(0, maxLength) + '...' : text
